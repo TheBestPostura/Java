3c7d391e7e696f1d9db1cbb77cdef86d36ddee3b
Moisés Ortiz Carmona <135473469+moisesortiz-dev@users.noreply.github.com>
2024-03-27 20:39:54 +0100

    Add files via upload

---
+++ 4 - Servlet y JSPs/5 - Headers/ManejoCabeceros/src/main/java/web/Servtlet.java
@@ -0,0 +1,33 @@
+
+package web;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Moises
+ */
+
+@WebServlet("/ServletHeaders")
+public class Servtlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String metodoHttp = request.getMethod();
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>Headers HTTP</title>");
+        out.print("</head>");
+        out.print("<body>");
+        out.print("<h1>Headers HTTP</h1>");
+        out.print("Metodo Http: " + metodoHttp);
+        out.print("</body>");
+        out.print("</html>");
+        out.close();
+    }
+}
